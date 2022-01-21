f8aa792170a15a49fc4ef6d24645e0019bfc5bfa
daoswap002 <daoswap@daoswap.cc>
2022-01-21 18:47:29 +0800

    update announcement

--- src/components/AnnouncementModal/index.tsx
+++ src/components/AnnouncementModal/index.tsx
@@ -51,111 +51,54 @@ function ConfirmationContent({ onDismiss }: { onDismiss: () => void }) {
         </RowBetween>
         <AutoColumn gap="auto" justify={'start'}>
           <Text fontWeight={500} fontSize={14} margin={10}>
-            {t('1, Hash and Competition Mining.')}
+            {t('1, Invitation release#58;')}
             <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('The 4th period of Arithmetic and Competitive Mining has ended on January 7, 2022 at 11#58;00.')}
+              {t('Will be available on January 23rd, 2022 at 11#58;00 am.')}
             </Text>
             <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('Statistical period#58; December 24, 2021 11#58;00 to January 7, 2022 11#58;00.')}
-            </Text>
-            <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('Statistics#58; Liquidity mining pool, number of single coin pledges (60%).')}
-            </Text>
-            <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('The reward has been issued on January 9th, 2022, please visit the official website to check.')}
+              {t('Directly invite planetary stellar to get DST at 20% of pledged LP value.')}
             </Text>
             <Text fontWeight={500} fontSize={14} margin={10}>
               {t(
-                'The 5th period of counting and competitive mining has started on January 7, 2022 at 11#58;00 and will end on January 21, 2022 at 11#58;00.'
+                'Directly invite single coin DAO pledge, according to 60% of the number of DAO pledge, and invite arithmetic unlock to get DST reward of 20% of LP value.'
               )}
             </Text>
             <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('empty info for cn 1')}
-            </Text>
-            <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('empty info for cn 2')}
+              {t(
+                "The DST reward for inviting unlocking will be transferred directly to the inviter's wallet address when pledged by the pledging party."
+              )}
             </Text>
           </Text>
           <Text fontWeight={500} fontSize={14} margin={10}>
-            {t('2, Fee share')}
+            {t('2, Hask and Competitive Mining#58;')}
             <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('For the first fee share, the rewarded pairs are#58;')}
-              <Text fontWeight={500} fontSize={14} margin={10}>
-                {t('USDT/DAO')}
-              </Text>
-              <Text fontWeight={500} fontSize={14} margin={10}>
-                {t('USDT/HFIL')}
-              </Text>
-              <Text fontWeight={500} fontSize={14} margin={10}>
-                {t('USDT/ETH')}
-              </Text>
+              {t('Phase 5 of Hask & Competition Mining has ended on January 21, 2022 at 11#58;00.')}
             </Text>
             <Text fontWeight={500} fontSize={14} margin={10}>
               {t(
-                'The total value of USDT is 23,798, which will be distributed to all current pledged one-year planetary/stellar nodes.'
-              )}
-            </Text>
-            <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('empty info for cn 3')}
-            </Text>
-            <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('Pick up address#58;')}{' '}
-              <StyledLink href={'https://fee.heco.daoswap.cc'}>https://fee.heco.daoswap.cc</StyledLink>
-            </Text>
-          </Text>
-          {/* <Text fontWeight={500} fontSize={14} margin={10}>
-            {t(
-              '3, Phase 3 liquidity mining officially starts&#58; Time&#58; December 27, 2021, 11&#58;00 am to January 10, 2022, 11&#58;00 am'
-            )}
-            <Text fontWeight={500} fontSize={14} margin={10}>
-              {t(
-                'Mining pools&#58; USDT-DAO, USDT-ETH, USDT-HFIL, USDT-HT, USDT-UNI, USDT-MDX, USDT-HBCH, USDT-HLTC, USDT-MANA, USDT-DOT, USDT-LINK'
+                'The 6th period of Arithmetic and Competition Mining has started on January 21, 2022 at 11#58;00 and will end on February 4, 2022 at 11#58;00.'
               )}
             </Text>
             <Text fontWeight={500} fontSize={14} margin={10}>
               {t(
-                'Note&#58; If you have not yet submitted your LPs for historical mining, you can submit them on the "Closed" page and then transfer them to the new liquidity mining pool.'
+                'The number of single coin pledges will also continue to count towards the count. The new part of the arithmetic value is calculated at 200%, and the unincreased or reduced arithmetic remains unchanged at 100% statistics.'
               )}
-            </Text>
-          </Text> */}
-          {/* <Text fontWeight={500} fontSize={14} margin={10}>
-            {t('2, About the node description&#58;')}
-            <Text fontWeight={500} fontSize={14} margin={10}>
-              {t(
-                '1, The status of planetary and stellar nodes will be determined by the number of DAO/USDT pool liquidity certificates (LPs) held.'
-              )}
-            </Text>
-            <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('2, Planetary nodes need >=1000 LP and Stellar nodes need >=10000 LP.')}
-            </Text>
-            <Text fontWeight={500} fontSize={14} margin={10}>
-              {t(
-                '3, the above criteria is valid within DAO price <= 30U, when >= 30U after the community vote to decide (whether to consider reducing the LP value criteria).'
-              )}
-            </Text>
-          </Text> */}
-          {/* <Text fontWeight={500} fontSize={14} margin={10}>
-            {t('4, Hash and competitive mining.')}
-            <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('The 3rd period of Hash and Competitive Mining has ended on December 24, 2021 at 11#58;00.')}
-            </Text>
-            <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('The statistical period is#58; December 10, 2021 11#58;00 to December 24, 2021 11#58;00.')}
-            </Text>
-            <Text fontWeight={500} fontSize={14} margin={10}>
-              {t('The reward has been issued on Dec 26th, 2021, please visit the official website to check.')}
             </Text>
           </Text>
           <Text fontWeight={500} fontSize={14} margin={10}>
-            {t(
-              'The 4th period of hash power and competitive mining has started on December 24, 2021 11#58;00 and will end on January 7, 2022 11#58;00.'
-            )}
+            {t('3, Task 6#58;')}
+            <Text fontWeight={500} fontSize={14} margin={10}>
+              {t(
+                'Starting from the 5th arithmetic power and competition mining, perform task 6#58; reward the number of nodes added within the team according to the team of the node identity wallet address (this node must be pledged for 1 year, not pledged for 1 year will not be added as new. (Also existing Genesis and seed nodes are excluded). '
+              )}
+            </Text>
+            <Text fontWeight={500} fontSize={14} margin={10}>
+              {t('For more information, please check#58;')}{' '}
+              <StyledLink href={'https://docs.daoswap.cc/jiguangxueyuan/'}>
+                https://docs.daoswap.cc/jiguangxueyuan/
+              </StyledLink>
+            </Text>
           </Text>
-          <Text fontWeight={500} fontSize={14} margin={10}>
-            {t(
-              'The added part of the arithmetic value is calculated at 200%, and the unincreased or reduced arithmetic power is still counted at 100% unchanged.'
-            )}
-          </Text> */}
         </AutoColumn>
       </Section>
     </Wrapper>
